2cb5f55f5c695944985a2227396648ae9fffb512
tom-jr <tomjavastdy@gmail.com>
2021-10-28 06:20:43 -0300

    REST testado

---
+++ curso/src/main/java/com/tom/curso/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.tom.curso.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String lista(){
+        return "Rest funcionando!";
+    }
+}
